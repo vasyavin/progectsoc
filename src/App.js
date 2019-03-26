2417ecc98cb5101e3ae1244a70f2c6a4592e67b9
vasyavin <vasyavin17@gmail.com>
2019-03-26 16:32:12 +0200

    lesson 19

--- src/App.js
+++ src/App.js
@@ -4,18 +4,27 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 const App = () => {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Navbar/>
-            <div className='app-wrapper-content'>
-                <Dialogs/>
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path='/Dialogs' component={Dialogs}/>
+                    <Route path='/Profile' component={Profile}/>
+                    <Route path='/News' component={News}/>
+                    <Route path='/Music' component={Music}/>
+                    <Route path='/Settings' component={Settings}/>
+                </div>
             </div>
-            {/*<Profile />*/}
-
-        </div>);
+        </BrowserRouter>
+    )
 }
 
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import s from './Navbar.module.css';
-console.log(s);
-// let s = {
-//     'nav': 'Navbar_nav__3ou9Q',
-//     'item': 'Navbar_item__3qaF3',
-//     'active' : 'Baksndakdn_actve'
- // }
+import Dialogs from "../Dialogs/Dialogs";
+
+
 
 let c1 = "item";
 let c2 = "active";
@@ -17,19 +14,19 @@ let classesNew = `${s.item} ${c2}`;
 const Navbar = () => {
     return <nav className={s.nav}>
         <div className={s.item}>
-            <a>Profile</a>
+            <a href='Profile'>Profile</a>
         </div>
         <div className={`${s.item} ${s.active}`}>
-            <a>Messages</a>
+            <a href='Dialogs'>Messages</a>
         </div>
         <div className={s.item}>
-            <a>News</a>
+            <a href='News'>News</a>
         </div>
         <div className={s.item}>
-            <a>Music</a>
+            <a href='Music'>Music</a>
         </div>
         <div className={s.item}>
-            <a>Settings</a>
+            <a href='Settings'>Settings</a>
         </div>
     </nav>
 }
