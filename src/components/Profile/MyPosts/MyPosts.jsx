3006fdab633c85da3d6cf34586ec90598403cb55
vasyavin <vasyavin17@gmail.com>
2019-03-26 12:59:28 +0200

    lesson 18

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,19 +3,19 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = (props) => {
-  return (
-    <div>
-      My posts
-      <div>
-        <textarea></textarea>
-        <button>Add post</button>
-      </div>
-      <div className={s.posts}>
-        <Post message='Hi, how are you?' likesCount='0'/>
-        <Post message="It's my first post" likesCount='23' />
-      </div>
-    </div>
-  )
+    return (
+        <div>
+            My posts
+            <div>
+                <textarea></textarea>
+                <button>Add post</button>
+            </div>
+            <div className={s.posts}>
+                <Post message='Hi, how are you?' likesCount='0'/>
+                <Post message="It's my first post" likesCount='23'/>
+            </div>
+        </div>
+    )
 }
 
 export default MyPosts;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,17 +3,18 @@ import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
-  return (
-    <div className={s.content}>
-      <div>
-        <img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350' />
-      </div>
-      <div>
-        ava + description
-      </div>
-      <MyPosts />
-    </div>
-  )
+    return (
+        <div>
+            <div>
+                <img
+                    src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350'/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <MyPosts/>
+        </div>
+    )
 }
 
 export default Profile;
