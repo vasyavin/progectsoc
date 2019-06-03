e332ebe7befa8a5010bc69d4f5543519ade05bfc
vasyavin <vasyavin17@gmail.com>
2019-06-03 11:36:59 +0300

    lesson 37

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
-import state, {subscribe} from "./redux/state";
+import store from "./redux/state";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import {addPost, updateNewPostText} from './redux/state';
-
-
-//addPost("vasya Good luck");
 let rerenderEntireTree = (state) => {
 
-    ReactDOM.render(<App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>,
+    ReactDOM.render(<App state={state}
+                         addPost={store.addPost.bind(store)}
+                         updateNewPostText={store.updateNewPostText.bind(store)}/>,
         document.getElementById('root'));
 
 }
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,75 +1,91 @@
-let rerenderEntireTree = () => {
-    console.log('input you text');
-}
-let state = {
+let store = {
+    _state: {
 
-    ProfilePage: {
+        ProfilePage: {
 
-        Posts: [
-            {id: 1, message: 'Hi, how are you?', likesCount: '0'},
-            {id: 2, message: 'It\'s my first post', likesCount: '23'}
-        ],
-        newPostText: 'Input you text'
+            Posts: [
+                {id: 1, message: 'Hi, how are you?', likesCount: '0'},
+                {id: 2, message: 'It\'s my first post', likesCount: '23'}
+            ],
+            newPostText: 'Input you text'
+
+        },
+
+        DialogsPage: {
+
+            imageProfile: [
+
+                {
+                    id: 1,
+                    img: 'https://zabavnik.club/wp-content/uploads/2018/05/Kartinki_na_avu_dlya_pacanov_2_14075531.jpg'
+                },
+                {
+                    id: 2,
+                    img: 'https://zabavnik.club/wp-content/uploads/2018/05/Kartinki_na_avu_dlya_pacanov_2_14075531.jpg'
+                },
+                {
+                    id: 3,
+                    img: 'https://zabavnik.club/wp-content/uploads/2018/05/Kartinki_na_avu_dlya_pacanov_2_14075531.jpg'
+                },
+                {id: 4, img: 'https://zabavnik.club/wp-content/uploads/2018/05/Kartinki_na_avu_dlya_pacanov_2_14075531.jpg'}
+            ],
+
+            dialogs: [
+                {id: 1, name: 'Vasya'},
+                {id: 2, name: 'Sasha'},
+                {id: 3, name: 'Pavlo'},
+                {id: 4, name: 'Vova'}
+            ],
+
+            Massages: [
+                {id: 1, massage: 'Hi'},
+                {id: 2, massage: 'How are you?'},
+                {id: 3, massage: 'Good'},
+                {id: 4, massage: 'Good luck'}
+            ]
+        }
+    },
+    getState() {
+        return this._state;
+    },
+    addPost() {
+        let newPost = {
+            id: 3,
+            message: this._state.ProfilePage.newPostText,
+            likesCount: 0
+        };
+
+        this._state.ProfilePage.Posts.push(newPost);
+        this._state.ProfilePage.newPostText = ('');
+        this._callSubscriber(this._state);
 
     },
+    _callSubscriber() {
+        console.log('input you text');
+    },
+    addPost() {
+        let newPost = {
+            id: 3,
+            message: this._state.ProfilePage.newPostText,
+            likesCount: 0
+        };
 
-    DialogsPage: {
+        this._state.ProfilePage.Posts.push(newPost);
+        this._state.ProfilePage.newPostText = ('');
+        this._callSubscriber(this._state);
 
-        imageProfile: [
+    },
+    updateNewPostText(newText) {
 
-            {
-                id: 1,
-                img: 'https://zabavnik.club/wp-content/uploads/2018/05/Kartinki_na_avu_dlya_pacanov_2_14075531.jpg'
-            },
-            {
-                id: 2,
-                img: 'https://zabavnik.club/wp-content/uploads/2018/05/Kartinki_na_avu_dlya_pacanov_2_14075531.jpg'
-            },
-            {
-                id: 3,
-                img: 'https://zabavnik.club/wp-content/uploads/2018/05/Kartinki_na_avu_dlya_pacanov_2_14075531.jpg'
-            },
-            {id: 4, img: 'https://zabavnik.club/wp-content/uploads/2018/05/Kartinki_na_avu_dlya_pacanov_2_14075531.jpg'}
-        ],
+        this._state.ProfilePage.newPostText = newText;
+        this._callSubscriber(this._state);
 
-        dialogs: [
-            {id: 1, name: 'Vasya'},
-            {id: 2, name: 'Sasha'},
-            {id: 3, name: 'Pavlo'},
-            {id: 4, name: 'Vova'}
-        ],
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
 
-        Massages: [
-            {id: 1, massage: 'Hi'},
-            {id: 2, massage: 'How are you?'},
-            {id: 3, massage: 'Good'},
-            {id: 4, massage: 'Good luck'}
-        ]
     }
 }
 
-export const addPost = () => {
-    let newPost = {
-        id: 3,
-        message: state.ProfilePage.newPostText,
-        likesCount: 0
-    };
-
-    state.ProfilePage.Posts.push(newPost);
-    state.ProfilePage.newPostText = ('');
-    rerenderEntireTree(state);
-
-}
-
-export const updateNewPostText = (newText) => {
-
-    state.ProfilePage.newPostText = newText;
-    rerenderEntireTree(state);
-
-}
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-}
-
-export default state;
+window.store = store;
+export default store;
